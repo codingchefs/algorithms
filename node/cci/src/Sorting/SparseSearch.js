b0fbd0be780f1d1a9e3abbfc960686d541591fb6
Bandaru Venkata Raghava, Sunil Kumar Red <sraghava3@gatech.edu>
2022-08-06 12:22:23 -0400

    Today's work - cci sorting problems

--- node/cci/src/Sorting/SparseSearch.js
+++ node/cci/src/Sorting/SparseSearch.js
@@ -1,7 +1,7 @@
 /**
  * Given a sorted array of strings that is interspersed with empty strings, write a method to find the location of a given string.
  * Example:
- * Input: ball, {"at", "", "", "", "ball", "", "", "car", "", "", "dad", "", ""}
+ * Input: ball, ["at", "", "", "", "ball", "", "", "car", "", "", "dad", "", ""]
  * Solution: Modify the regular binary search so that we move left or right of mid if mid element is "" till we find a
  * non empty string
  */
@@ -18,10 +18,12 @@ const SparseSearchImpl = (array, low, high, value) => {
   // base case
   if (low > high) return -1;
 
+  // mid point
   let mid = Math.floor((low + high) / 2);
 
-  // Find non empty mid point
+  // check if mid element is empty string
   if (array[mid] === "") {
+    // initialize left and right
     let left = mid - 1;
     let right = mid + 1;
 
@@ -29,27 +31,30 @@ const SparseSearchImpl = (array, low, high, value) => {
       // end condition
       if (left < low && right > high) return -1;
 
+      // check if mid element is non empty for left
       if (left >= low && array[left] !== "") {
         mid = left;
         break;
+        // check if mid element is non empty for right
       } else if (right <= high && array[right] !== "") {
         mid = right;
         break;
       }
-
+      // change left and right
       left--;
-      right++
+      right++;
     }
   }
 
   // perform normal binary search
   if (array[mid] === value) {
     return mid;
-  } else if (value < array[mid]) {
+  } else if (array[mid] > value) {
     return SparseSearchImpl(array, low, mid - 1, value);
   } else {
     return SparseSearchImpl(array, mid + 1, high, value);
   }
+
 };
 
 module.exports = SparseSearch;
