9ee2040d2277b3590705cad23304f0b4417a2a83
Bandaru Venkata Raghava, Sunil Kumar Red <sraghava3@gatech.edu>
2022-08-21 08:20:07 -0400

    Today's work - 1 RP

--- node/cracking-the-coding-interview/src/chapter-8-recursion-and-dynamic-programming/5-RecursiveMultiply.js
+++ node/cracking-the-coding-interview/src/chapter-8-recursion-and-dynamic-programming/5-RecursiveMultiply.js
@@ -1,6 +1,9 @@
 /**
  * Recursive Multiply: Write a recursive function to multiply two positive integers without using the * operator (or /
  * operator). You can use addition, subtraction, and bit shifting, but you should minimize the number of those operations.
+ *
+ * Solution: we could count half the squares and then double it (by adding this count to itself). this "doubling" only
+ * works if the number is in fact even. When it's not even, we need to do the counting/summing from scratch.
  */
 
 const minProduct = (a, b) => {
@@ -13,6 +16,7 @@ const minProductHelper = (smaller, bigger) => {
   if (smaller === 0) return 0;
   else if (smaller === 1) return bigger;
 
+  // divide by 2
   const s = smaller >> 1;
   const halfProd = minProductHelper(s, bigger);
 
