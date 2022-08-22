6bcb7932f6671142ae51bfccf442de2dfaa10887
Bandaru Venkata Raghava, Sunil Kumar Red <sraghava3@gatech.edu>
2022-08-21 22:13:59 -0400

    Today's work - 1 RP

--- node/cracking-the-coding-interview/tests/chapter-80-recursion-and-dynamic-programming/5-RecursiveMultiplyTest.js
+++ node/cracking-the-coding-interview/tests/chapter-80-recursion-and-dynamic-programming/5-RecursiveMultiplyTest.js
@@ -4,9 +4,30 @@ const RecursiveMultiply = require('../../src/chapter-8-recursion-and-dynamic-pro
 
 describe('Recursive Multiply', () => {
 
-  it('Given two positive integers, return result of multiply', () => {
+  it('Given two positive integers with one of them 0, return 0 as result of multiply', () => {
+    const expectedResult = 0;
+    const result = RecursiveMultiply(12, 0);
+
+    expect(result).to.deep.equal(expectedResult);
+  });
+
+  it('Given two positive integers with one of them 1, return other number as result of multiply', () => {
+    const expectedResult = 20;
+    const result = RecursiveMultiply(1, 20);
+
+    expect(result).to.deep.equal(expectedResult);
+  });
+
+  it('Given two positive integers with one of them odd, return result of multiply', () => {
     const expectedResult = 60;
-    const result = RecursiveMultiply(12, 5);
+    const result = RecursiveMultiply(3, 20);
+
+    expect(result).to.deep.equal(expectedResult);
+  });
+
+  it('Given two positive integers, return result of multiply', () => {
+    const expectedResult = 6000;
+    const result = RecursiveMultiply(120, 50);
 
     expect(result).to.deep.equal(expectedResult);
   });
