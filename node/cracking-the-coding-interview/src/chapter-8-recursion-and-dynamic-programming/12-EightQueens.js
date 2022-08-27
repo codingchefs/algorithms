ea1db151335a85c268959ba4aa8ff4980db8688c
Bandaru Venkata Raghava, Sunil Kumar Red <sraghava3@gatech.edu>
2022-08-27 07:46:54 -0400

    Today's work - 1 RP

--- node/cracking-the-coding-interview/src/chapter-8-recursion-and-dynamic-programming/12-EightQueens.js
+++ node/cracking-the-coding-interview/src/chapter-8-recursion-and-dynamic-programming/12-EightQueens.js
@@ -4,20 +4,27 @@
  * bisect the board.
  */
 const solveNQueens = (n) => {
+  // initialize col, diag sets
   const col = new Set();
   const posDiag = new Set();
   const negDiag = new Set();
 
+  // initialize result
   const result = []
+  // create board
   const board = Array(n).fill(0).map(_ => Array(n).fill('.'))
 
+  // backtrack recursive method
   const backtrack = (r) => {
+    // check if r === n, push result
     if (r === n) {
       result.push(board.map(row => row.join('')))
       return
     }
 
+    // loop through columns
     for(let c = 0; c < n; c++) {
+      // if col has no place to fill, skip
       if (col.has(c) || posDiag.has(r + c) || negDiag.has(r - c)) {
         continue;
       }
@@ -28,8 +35,10 @@ const solveNQueens = (n) => {
       negDiag.add(r -c );
       board[r][c] = 'Q';
 
+      // call backtrack with next col
       backtrack(r + 1);
 
+      // cleanup
       col.delete(c);
       posDiag.delete(r + c);
       negDiag.delete(r -c );
