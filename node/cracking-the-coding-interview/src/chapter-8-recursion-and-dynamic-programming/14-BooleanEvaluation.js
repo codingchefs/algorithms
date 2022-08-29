3684e88466790438e180618a797ab9c464e1f37b
Bandaru Venkata Raghava, Sunil Kumar Red <sraghava3@gatech.edu>
2022-08-28 22:21:02 -0400

    Today's work - 1 RP

---
+++ node/cracking-the-coding-interview/src/chapter-8-recursion-and-dynamic-programming/14-BooleanEvaluation.js
@@ -0,0 +1,10 @@
+/**
+ * // TODO revisit later
+ * Boolean Evaluation: Given a boolean expression consisting of the symbols 0 (false), 1 (true), & (AND), I (OR),
+ * and /\ (XOR), and a desired boolean result value result, implement a function to count the number of ways of
+ * parenthesizing the expression such that it evaluates to result. The expressionshouldbefullyparenthesized
+ * (e.g.,(0)A(1))but not extraneously(e.g.,(((0))/\(1))).
+ * EXAMPLE
+ * countEval("l^0|0|1", false) -> 2
+ * countEval("0&0&0&1^1|0", true)-> 10
+ */
