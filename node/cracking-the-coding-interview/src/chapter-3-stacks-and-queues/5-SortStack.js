70d6a272f79d9713b29f0c45def8000d49f353ea
Bandaru Venkata Raghava, Sunil Kumar Red <sraghava3@gatech.edu>
2022-09-03 16:27:55 -0400

    Today's work - 1 Stack Problem

--- node/cracking-the-coding-interview/src/chapter-3-stacks-and-queues/5-SortStack.js
+++ node/cracking-the-coding-interview/src/chapter-3-stacks-and-queues/5-SortStack.js
@@ -2,24 +2,32 @@
  * Sort Stack: Write a program to sort a stack such that the smallest items are on the top. You can use an additional
  * temporary stack, but you may not copy the elements into any other data structure (such as an array). The stack
  * supports the following operations: push, pop, peek, and isEmpty.
+ * solution: Create a tempStack, swap the numbers from currentStack to tempStack if number on tempStack is less than the
+ * one on current stack.
  */
 
 const Stack = require('./Stack');
 const SortStack = (currentStack) => {
+  // create a temp stack
   const tempStack = new Stack();
+  // loop through current stack
   while (!currentStack.isEmpty()) {
-    // Insert each element in s in sorted order into r
+    // get the first value as temp variable
     const tmp = currentStack.pop();
+    // loop through temp array and also check if tmp < temp array top
     while (!tempStack.isEmpty() && tempStack.peek() < tmp) {
+      // add to temp stack
       currentStack.push(tempStack.pop());
     }
+
+    // add to temp stack
     tempStack.push(tmp);
   }
-
-  // copy elements from r back into s
-  while(!tempStack.isEmpty()) {
+  // loop through temp stack and add to current stack
+  while (!tempStack.isEmpty()) {
     currentStack.push(tempStack.pop());
   }
+
 };
 
 module.exports = SortStack;
