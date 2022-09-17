fdf9b1fbd1bbe67ab775501b5b6e3271e2401895
Bandaru Venkata Raghava, Sunil Kumar Red <sraghava3@gatech.edu>
2022-09-17 07:05:32 -0400

    1 Problem - String & Arrays

--- node/cracking-the-coding-interview/src/chapter-1-arrays-and-strings/3-Urlify.js
+++ node/cracking-the-coding-interview/src/chapter-1-arrays-and-strings/3-Urlify.js
@@ -2,46 +2,40 @@
  * URLify: Write a method to replace all spaces in a string with '%20:
  * You may assume that the string has sufficient space at the end to hold the additional characters,
  * and that you are given the "true" length of the string.
- * (Note: if implementing in Java, please use a character array so that you can perform this operation in place.)
-    EXAMPLE
-    Input: "Mr John Smith    ", 13
-    Output: "Mr%2eJohn%2eSmith"
- solution: loop once to get the number of spaces. allocate additional x2 spaces into the array.
- loop once again to start the ending characters towards the array end. If space is there replace with 0 2 %.
+ * EXAMPLE
+ * Input: "Mr John Smith    ", 13
+ * Output: "Mr%2eJohn%2eSmith"
+ *
+ * solution: loop once to get the number of spaces. allocate additional x2 spaces into the array.
+ * loop once again to start the ending characters towards the array end. If space is there replace with 0 2 %.
  **/
 
 const urlify = (str1, trueLength) => {
   let spaceCount = 0;
   let str = str1.split('');
   // 1st loop check number of spaces
-  for (let i=0; i < trueLength; i++) {
-      if(str[i] === ' ') {
-          spaceCount++;
-      }
+  for (let i = 0; i < trueLength; i++) {
+    if (str[i] === ' ') {
+      spaceCount++;
+    }
   }
-  let index = trueLength + spaceCount * 2;
-  if(trueLength < str.length) str[trueLength] = '\0';
+  let index = trueLength + (spaceCount * 2);
+  if (trueLength < str.length) str[trueLength] = '\0';
   // move the end of characters towards end
-  for(let i = trueLength - 1; i>0; i--){
-      if(str[i] === ' ') {
-          str[index - 1] = '0';
-          str[index - 2] = '2';
-          str[index - 3] = '%';
-          index = index - 3;
-      } else {
-          // copy the index char to ith char
-          str[index - 1] = str[i];
-          index--;
-      }
+  for (let i = trueLength - 1; i > 0; i--) {
+    if (str[i] === ' ') {
+      str[index - 1] = '0';
+      str[index - 2] = '2';
+      str[index - 3] = '%';
+      index = index - 3;
+    } else {
+      // copy the index char to ith char
+      str[index - 1] = str[i];
+      index--;
+    }
   }
+
   return str.join('');
-
 };
 
-// using javascript
-const urlify2 = (str, trueLength) => {
-    const actualStr = str.substr(0, trueLength);
-    return actualStr.split(' ').join('%20');
-};
-
-console.log(urlify2('Mr John Smith    ', 13));
+module.exports = urlify;
---
+++ node/cracking-the-coding-interview/tests/chapter-1-arrays-and-strings/3-UrlifyTest.js
@@ -0,0 +1,18 @@
+const chai = require('chai');
+const expect = chai.expect;
+const Urlify = require('../../src/chapter-1-arrays-and-strings/3-Urlify');
+
+describe("Arrays and Strings - Urlify", () => {
+
+  it("Given a strings with space and its true length, returns replace all spaces in a string with '%20'", function () {
+    const res = Urlify('Mr John Smith    ', 13);
+    expect(res).to.equal('Mr%20John%20Smith');
+  });
+
+  it("Given a strings with space and its true length less than actual length, returns replace all spaces in a string with '%20'", function () {
+    const res = Urlify('Mr John Smith    ', 10);
+    expect(res).to.equal('Mr%20John%20Smith');
+  });
+
+});
+
