4c39bf5720204b52f74e3971adfea91508180faa
Bandaru Venkata Raghava, Sunil Kumar Red <sraghava3@gatech.edu>
2022-09-18 11:59:06 -0400

    1 Problem - String & Arrays

--- node/cracking-the-coding-interview/src/chapter-1-arrays-and-strings/3-Urlify.js
+++ node/cracking-the-coding-interview/src/chapter-1-arrays-and-strings/3-Urlify.js
@@ -11,26 +11,34 @@
  **/
 
 const urlify = (str1, trueLength) => {
+  // count the spaces by iterating through full string
   let spaceCount = 0;
   let str = str1.split('');
-  // 1st loop check number of spaces
   for (let i = 0; i < trueLength; i++) {
-    if (str[i] === ' ') {
+    const currentChar = str[i];
+    if (currentChar === ' ') {
       spaceCount++;
     }
   }
+
+  // calculate full length
   let index = trueLength + (spaceCount * 2);
-  if (trueLength < str.length) str[trueLength] = '\0';
-  // move the end of characters towards end
-  for (let i = trueLength - 1; i > 0; i--) {
-    if (str[i] === ' ') {
+  if (trueLength < str.length) {
+    str = str.splice(0, trueLength);
+  }
+
+  // replace spaces with 0 2 % from backwards
+  for (let j = trueLength - 1; j > 0; j--) {
+    const currentChar = str[j];
+    if (currentChar === ' ') {
+      // add the chars 0, 2, % and decrement index by 3
       str[index - 1] = '0';
       str[index - 2] = '2';
       str[index - 3] = '%';
       index = index - 3;
     } else {
-      // copy the index char to ith char
-      str[index - 1] = str[i];
+      // add the currentChar and decrement index by 1
+      str[index - 1] = currentChar;
       index--;
     }
   }
--- node/cracking-the-coding-interview/tests/chapter-1-arrays-and-strings/3-UrlifyTest.js
+++ node/cracking-the-coding-interview/tests/chapter-1-arrays-and-strings/3-UrlifyTest.js
@@ -9,9 +9,14 @@ describe("Arrays and Strings - Urlify", () => {
     expect(res).to.equal('Mr%20John%20Smith');
   });
 
+  it("Given a strings with no spaces and its true length, returns replace all spaces in a string with '%20'", function () {
+    const res = Urlify('MrJohnSmith', 11);
+    expect(res).to.equal('MrJohnSmith');
+  });
+
   it("Given a strings with space and its true length less than actual length, returns replace all spaces in a string with '%20'", function () {
     const res = Urlify('Mr John Smith    ', 10);
-    expect(res).to.equal('Mr%20John%20Smith');
+    expect(res).to.equal('Mr%20John%20Sm');
   });
 
 });
