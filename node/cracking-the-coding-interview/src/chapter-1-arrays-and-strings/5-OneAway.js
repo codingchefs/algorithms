8d457ccd79edbb412d49fba23e9ef707ab52202a
Bandaru Venkata Raghava, Sunil Kumar Red <sraghava3@gatech.edu>
2022-09-18 15:35:03 -0400

    1 Problem - String & Arrays

--- node/cracking-the-coding-interview/src/chapter-1-arrays-and-strings/5-OneAway.js
+++ node/cracking-the-coding-interview/src/chapter-1-arrays-and-strings/5-OneAway.js
@@ -8,67 +8,12 @@ write a function to check if they are one edit (or zero edits) away.
  pale, bae -> false
 
  Solution:
- check if both string lengths are equal -> replace
- check if both strings lengths differ by 1, then either insert or removal
- if replace -> if a second difference is found, then return false
- if insert/removal -> if a second difference is found in consecutive order, then return false
- > takesO(n) time,where n is the length of the shorter string.
+ compare lengths are only different by 1.
+ Get shorter and longer strings
+ * compare each character and ensure that the strings are only different by one.
+ if chars are different, strings are same length increment index by 1. if chars are same, increment index by 1
+ always increment index 2 by 1
  */
-
-/**
- * if second character is found different, then return false
- */
-const oneReplaceAway = (s1, s2) => {
-    let diffFound = false;
-    for (let i = 0; i < s1.length; i++) {
-        if (s1[i] !== s2[i]) {
-            if (diffFound) {
-                return false;
-            }
-            diffFound = true;
-        }
-    }
-    return true;
-};
-
-/**
- * if a second consecutive letter is found different, then return false
- */
-const oneEditInsert = (s1, s2) => {
-    let index1 = 0;
-    let index2 = 0;
-    while (index1 < s1.length && index2 < s2.length) {
-        if (s1[index1] !== s2[index2]) {
-            if (index1 !== index2) {
-                return false;
-            }
-            index2++
-        } else {
-            index1++;
-            index2++;
-        }
-    }
-    return true;
-};
-
-/**
- * if first string length is equal to second string length, then its replace
- * if first string length is greater than second string length by 1, then its insert
- * if first string length is less than second string length by 1, then its removal
- * @param first
- * @param second
- * @returns {boolean}
- */
-const oneEditAway = (first, second) => {
-    if(first.length === second.length) {
-        return oneReplaceAway(first, second);
-    } else if(first.length + 1 === second.length) {
-        return oneEditInsert(first, second);
-    } else if(first.length - 1 === second.length) {
-        return oneEditInsert(second, first);
-    }
-    return false;
-};
 
 /**
  * both methods combined to one
@@ -76,31 +21,31 @@ const oneEditAway = (first, second) => {
  * @param second
  * @returns {boolean}
  */
-const oneEditAway2 = (first, second) => {
-    // false condition
-    if (Math.abs(first.length - second.length) > 1) return false;
+const oneEditAway = (first, second) => {
+  // false condition
+  if (Math.abs(first.length - second.length) > 1) return false;
 
-    // get shorter and longer string
-    let s1 = first.length < second.length ? first : second;
-    let s2 = first.length < second.length ? second : first;
+  // get shorter and longer string
+  let s1 = first.length < second.length ? first : second;
+  let s2 = first.length < second.length ? second : first;
 
-    let index1 = 0;
-    let index2 = 0;
-    let foundDifference = false;
+  let index1 = 0;
+  let index2 = 0;
+  let foundDifference = false;
 
-    while (index2 < s2.length && index1 < s1.length) {
-        if (s1[index1] !== s2[index2]) {
-            // ensure that this is the first difference found
-            if (foundDifference) return false;
-            foundDifference = true;
-            if (s1.length === s2.length) index1++;
-        } else {
-            index1++; // if matching, move shorter pointer
-        }
-        index2++; // always move pointer for longer string
+  while (index2 < s2.length && index1 < s1.length) {
+    if (s1[index1] !== s2[index2]) {
+      // ensure that this is the first difference found
+      if (foundDifference) return false;
+      foundDifference = true;
+      if (s1.length === s2.length) index1++;
+    } else {
+      index1++; // if matching, move shorter pointer
     }
-    return true;
+    index2++; // always move pointer for longer string
+  }
+  return true;
 };
 
-module.exports = oneEditAway2;
+module.exports = oneEditAway;
 
