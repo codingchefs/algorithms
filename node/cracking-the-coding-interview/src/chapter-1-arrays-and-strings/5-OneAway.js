07a5da7f9b9e6ea96fea247167c47fb4a525eecc
Bandaru Venkata Raghava, Sunil Kumar Red <sraghava3@gatech.edu>
2022-09-18 15:46:55 -0400

    1 Problem - String & Arrays

--- node/cracking-the-coding-interview/src/chapter-1-arrays-and-strings/5-OneAway.js
+++ node/cracking-the-coding-interview/src/chapter-1-arrays-and-strings/5-OneAway.js
@@ -22,28 +22,35 @@ write a function to check if they are one edit (or zero edits) away.
  * @returns {boolean}
  */
 const oneEditAway = (first, second) => {
-  // false condition
+  // check lengths to be no more than 1
   if (Math.abs(first.length - second.length) > 1) return false;
 
-  // get shorter and longer string
-  let s1 = first.length < second.length ? first : second;
-  let s2 = first.length < second.length ? second : first;
+  // get shorter and longer strings
+  const shorter = first.length > second.length ? second : first;
+  const longer = first.length > second.length ? first : second;
 
-  let index1 = 0;
-  let index2 = 0;
+  let shortIndex = 0;
+  let longIndex = 0;
   let foundDifference = false;
 
-  while (index2 < s2.length && index1 < s1.length) {
-    if (s1[index1] !== s2[index2]) {
-      // ensure that this is the first difference found
-      if (foundDifference) return false;
+  // loop through while comparing each character
+  while (shortIndex < shorter.length && longIndex < longer.length) {
+    // if chars are different, then mark the difference
+    if (shorter[shortIndex] !== longer[longIndex]) {
+      if (foundDifference) {
+        return false;
+      }
       foundDifference = true;
-      if (s1.length === s2.length) index1++;
+      // if strings same length, increment short index
+      if (shorter.length === longer.length) shortIndex++;
     } else {
-      index1++; // if matching, move shorter pointer
+      // else increment short index
+      shortIndex++;
     }
-    index2++; // always move pointer for longer string
+    // always increment long index
+    longIndex++;
   }
+
   return true;
 };
 
