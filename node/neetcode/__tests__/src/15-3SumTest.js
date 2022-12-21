9386b9608d31cff61ff1486a685a192c1a752020
Bandaru, Sunil Kumar <codingchefs@gmail.com>
2022-12-21 05:47:22 -0500

    12/21 1 problem

--- node/neetcode/__tests__/src/15-3SumTest.js
+++ node/neetcode/__tests__/src/15-3SumTest.js
@@ -1,10 +1,7 @@
 const _15_3Sum = require("../../src/15-3Sum")
 // @ponicode
 describe("_15_3Sum", () => {
-  test("-1", () => {
-    expect(true).toBeTruthy()
-  })
-  /*test("0", () => {
+  test("0", () => {
         let result = _15_3Sum([-1,0,1,2,-1,-4])
         expect(result).toEqual([[-1,-1,2],[-1,0,1]])
     })
@@ -17,6 +14,10 @@ describe("_15_3Sum", () => {
     test("2", () => {
         let result = _15_3Sum([0,0,0])
         expect(result).toEqual([[0,0,0]])
-    })*/
+    })
 
+    test("3", () => {
+      let result = _15_3Sum([-2, -2, 0, 0, 2, 2])
+      expect(result).toEqual([[-2,0,2]])
+    })
 })
--- node/neetcode/src/15-3Sum.js
+++ node/neetcode/src/15-3Sum.js
@@ -2,6 +2,8 @@
  * Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k,
  * and nums[i] + nums[j] + nums[k] == 0.
  * Notice that the solution set must not contain duplicate triplets.
+ * Time complexity: O(n^2)
+ * Space complexity: O(1) or O(n) based on sorting implementation
  */
 
 /**
@@ -9,7 +11,47 @@
  * @return {number[][]}
  */
 const threeSum = (nums) => {
+  // initialize result
+  const result = [];
+  // sort numbers
+  nums.sort();
 
+  // iterate through numbers
+  for (let i = 0; i < nums.length; i++) {
+    // initialize a
+    const currentNumber = nums[i];
+    // if repeated value found, then skip the loop
+    if (i > 0 && currentNumber === nums[i - 1]) {
+      continue;
+    }
+
+    // initialize left and right
+    let left = i + 1;
+    let right = nums.length - 1;
+    // -2, -2, 0, 0, 2, 2
+    let threeSum;
+    // iterate till left < right
+    while (left < right) {
+      // calculate threeSum
+      threeSum = currentNumber + nums[left] + nums[right];
+      // if threeSum greater than 0, decrease right
+      if (threeSum > 0) {
+        right -= 1;
+      // if threeSum less than 0, increase left
+      } else if (threeSum < 0) {
+        left += 1;
+      } else {
+      // add result
+        result.push([currentNumber, nums[left], nums[right]]);
+        left += 1;
+        while (nums[left] === nums[left - 1] && left < right) {
+          left += 1;
+        }
+      }
+    }
+  }
+
+  return result;
 };
 
 module.exports = threeSum;
