59fbda739af67ac904273a7bc07618f23f54c2ac
Bandaru, Sunil Kumar <codingchefs@gmail.com>
2022-12-21 05:48:03 -0500

    12/21 1 problem

--- node/neetcode/__tests__/src/15-3SumTest.js
+++ node/neetcode/__tests__/src/15-3SumTest.js
@@ -2,22 +2,22 @@ const _15_3Sum = require("../../src/15-3Sum")
 // @ponicode
 describe("_15_3Sum", () => {
   test("0", () => {
-        let result = _15_3Sum([-1,0,1,2,-1,-4])
-        expect(result).toEqual([[-1,-1,2],[-1,0,1]])
-    })
+    let result = _15_3Sum([-1, 0, 1, 2, -1, -4])
+    expect(result).toEqual([[-1, -1, 2], [-1, 0, 1]])
+  })
 
-    test("1", () => {
-        let result = _15_3Sum([0,1,1])
-        expect(result).toEqual([])
-    })
+  test("1", () => {
+    let result = _15_3Sum([0, 1, 1])
+    expect(result).toEqual([])
+  })
 
-    test("2", () => {
-        let result = _15_3Sum([0,0,0])
-        expect(result).toEqual([[0,0,0]])
-    })
+  test("2", () => {
+    let result = _15_3Sum([0, 0, 0])
+    expect(result).toEqual([[0, 0, 0]])
+  })
 
-    test("3", () => {
-      let result = _15_3Sum([-2, -2, 0, 0, 2, 2])
-      expect(result).toEqual([[-2,0,2]])
-    })
+  test("3", () => {
+    let result = _15_3Sum([-2, -2, 0, 0, 2, 2])
+    expect(result).toEqual([[-2, 0, 2]])
+  })
 })
