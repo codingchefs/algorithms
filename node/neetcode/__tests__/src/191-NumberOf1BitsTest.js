cf17b4d9804077b69b2797fdf09505399056350f
Bandaru, Sunil Kumar <codingchefs@gmail.com>
2022-12-26 09:14:52 -0500

    12/26 - 1 Problem

--- node/neetcode/__tests__/src/191-NumberOf1BitsTest.js
+++ node/neetcode/__tests__/src/191-NumberOf1BitsTest.js
@@ -1,22 +1,20 @@
-const _191_NumberOf1Bits = require("../../src/191-NumberOf1Bits")
+const { HammingWeight_2 } = require("../../src/191-NumberOf1Bits")
 // @ponicode
 describe("_191_NumberOf1Bits", () => {
-  test("-1", () => {
-    expect(true).toBeTruthy()
-  })
-  /*test("0", () => {
-        let result = _191_NumberOf1Bits(0b00000000000000000000000000001011)
+
+  test("0", () => {
+        let result = HammingWeight_2(0b00000000000000000000000000001011)
         expect(result).toBe(3)
     })
 
     test("1", () => {
-        let result = _191_NumberOf1Bits(0b00000000000000000000000010000000)
+        let result = HammingWeight_2(0b00000000000000000000000010000000)
         expect(result).toBe(1)
     })
 
     test("2", () => {
-        let result = _191_NumberOf1Bits(0b11111111111111111111111111111101)
+        let result = HammingWeight_2(0b11111111111111111111111111111101)
         expect(result).toBe(31)
-    })*/
+    })
 
 })
--- node/neetcode/src/191-NumberOf1Bits.js
+++ node/neetcode/src/191-NumberOf1Bits.js
@@ -7,13 +7,45 @@
  * is the same, whether it is signed or unsigned.
  * In Java, the compiler represents the signed integers using 2's complement notation. Therefore, in Example 3, the
  * input represents the signed integer. -3.
+ *
+ * Solutions:
+ * 1) iterate while right shift of number is not zero, mod by 2 and is result is 1, increment the result
+ * 2) A more efficient way to reduce number of loops is to use the trick n & (n-1). and repeat loop till it is all zeros
  */
 /**
+ * Time complexity O(1)
  * @param {number} n - a positive integer
  * @return {number}
  */
 const HammingWeight = (n) => {
-
+  let result = 0;
+  // iterate while number right shift not equals zero
+  while (n) {
+    // mod by 2 and if result is 1, then count in
+    result += n % 2;
+    n = n >> 1;
+  }
+  return result;
 };
 
-module.exports = HammingWeight;
+/**
+ * This solutions is more efficient with less loops
+ * Time complexity O(1)
+ * @param n
+ * @return {number}
+ * @constructor
+ */
+const HammingWeight_2 = (n) => {
+  let result = 0;
+  // iterate while number right shift not equals zero
+  while (n) {
+    n = n & (n - 1);
+    result += 1;
+  }
+  return result;
+};
+
+module.exports = {
+  HammingWeight,
+  HammingWeight_2,
+}
