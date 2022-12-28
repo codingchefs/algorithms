24d4891067907d710bb7f9d94fac4158315dbf6d
Bandaru, Sunil Kumar <codingchefs@gmail.com>
2022-12-28 08:18:42 -0500

    12/28 - 1 Problem

--- node/neetcode/src/75-MissingNumber.js
+++ node/neetcode/src/75-MissingNumber.js
@@ -1,6 +1,10 @@
 /**
  * Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range
  * that is missing from the array.
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ *
+ * Solution: add sum from 0 to len(nums) and subtract sum of nums and return the result. Make it in one loop.
  */
 
 /**
@@ -8,7 +12,13 @@
  * @return {number}
  */
 const MissingNumber = (nums) => {
+  let result = nums.length;
+  // (3, 0, 1, 2) - (3, 0 , 1)
+  for (let i = 0; i < nums.length; i++) {
+    result += (i - nums[i]);
+  }
 
+  return result;
 };
 
 module.exports = MissingNumber;
