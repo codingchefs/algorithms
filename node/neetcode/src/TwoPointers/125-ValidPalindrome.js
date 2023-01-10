e4a182c5c43b4f4c4ba3824593c10cfaea8d7cb6
Bandaru, Sunil Kumar <codingchefs@gmail.com>
2023-01-09 21:55:47 -0500

    1/9/2023 - 1 Problem

--- node/neetcode/src/TwoPointers/125-ValidPalindrome.js
+++ node/neetcode/src/TwoPointers/125-ValidPalindrome.js
@@ -16,7 +16,38 @@
  * @return {boolean}
  */
 const isPalindrome = (s) => {
+  // initialize left and right pointers
+  let left = 0;
+  let right = s.length - 1;
 
+  // loop through till left < right
+  while(left < right) {
+    // loop through till left < right && left is isAlphanumeric
+    while(left < right && !_isAlphanumeric(s[left])) {
+      left++;
+    }
+    // loop through till left < right && right is isAlphanumeric
+    while(left < right && !_isAlphanumeric(s[right])) {
+      right--;
+    }
+    // compare left char to right char
+    if(s[left].toLowerCase() !== s[right].toLowerCase()) {
+      return false;
+    }
+    // increment left and decrement right
+    left++;
+    right--;
+  }
+
+  return true;
+};
+
+const _isAlphanumeric = (s) => {
+  return (
+    "A".charCodeAt(0) <= s.charCodeAt(0) && s.charCodeAt(0) <= "Z".charCodeAt(0) || // 65-90
+    "a".charCodeAt(0) <= s.charCodeAt(0) && s.charCodeAt(0) <= "z".charCodeAt(0) || // 97 - 122
+    '0'.charCodeAt(0) <= s.charCodeAt(0) && s.charCodeAt(0) <= '9'.charCodeAt(0) // 48 - 57
+  );
 };
 
 module.exports = isPalindrome;
