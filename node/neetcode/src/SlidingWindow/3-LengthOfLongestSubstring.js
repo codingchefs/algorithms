c1b8b88f7cba6e94254432877bdda3e70ec0cc79
Bandaru, Sunil Kumar <codingchefs@gmail.com>
2023-01-22 15:54:31 -0500

    1/22/2023 - 1 Problem

--- node/neetcode/src/SlidingWindow/3-LengthOfLongestSubstring.js
+++ node/neetcode/src/SlidingWindow/3-LengthOfLongestSubstring.js
@@ -22,21 +22,25 @@
  * @return {number}
  */
 const LengthOfLongestSubstring = (s) => {
+  // initialize charSet, left and result
   let charSet = new Set();
-
   let left = 0;
   let result = 0;
 
   for (let right = 0; right < s.length; right++) {
+    // check if charset has right element
     while (charSet.has(s[right])) {
+      // delete the left character
       charSet.delete(s[left]);
-      left += 1;
+      left++;
     }
 
+    // add right char to CharSet
     charSet.add(s[right]);
-    result = Math.max(result, right - left + 1)
+    // calculate max
+    result = Math.max(result, right - left + 1);
   }
-  return result
+  return result;
 };
 
 module.exports = LengthOfLongestSubstring;
