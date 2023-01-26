804f3fe156d5c782d61610489cb8df5d19e1dd65
Bandaru, Sunil Kumar <codingchefs@gmail.com>
2023-01-25 22:36:48 -0500

    1/25/2023 - 1 Problem

--- node/neetcode/src/Stack/ValidParentheses.js
+++ node/neetcode/src/Stack/ValidParentheses.js
@@ -8,7 +8,11 @@
  * Every close bracket has a corresponding open bracket of the same type
  *
  * Solution:
- *
+ * maintain a closeToOpen Map, maintain a stack.
+ * iterate through the string,
+ * if it is a closing bracket, see if last element in stack is opening bracket and remove it.
+ * if it is a opening bracket, then add it to the stack
+ * Finally if the stack is empty return true
  * TimeComplexity: O(N)
  * SpaceComplexity: O(N)
  */
@@ -17,21 +21,23 @@
  * @return {boolean}
  */
 const IsValid = (s) => {
-  const closeToOpen = { ')': '(', '}' : '{', ']': '[' };
+  const closeToOpen = {')': '(', '}': '{', ']': '['};
   const stack = [];
-  for(c of s) {
-    if(closeToOpen[c]) {
-      if(stack.length && stack[stack.length - 1] === closeToOpen[c]) {
+
+  for (const currentChar of s) {
+    // its a closing bracket
+    if (closeToOpen[currentChar]) {
+      if (stack.length && stack[stack.length - 1] === closeToOpen[currentChar]) {
         stack.pop();
       } else {
         return false;
       }
     } else {
-      stack.push(c);
+      stack.push(currentChar);
     }
   }
 
-  return (!stack.length)
+  return (!stack.length);
 };
 
 module.exports = IsValid;
